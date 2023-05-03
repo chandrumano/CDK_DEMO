73193306e02b1dd2e7a4f4891300ba7d1b203ed3
Chandru Manoharan <chandira.s.manoharan@avanade.com>
2023-05-03 16:34:59 -0400

    First commit

--- lib/cdk_demo-stack.ts
+++ lib/cdk_demo-stack.ts
@@ -6,6 +6,10 @@ export class CdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const bucket = new cdk.aws_s3.Bucket(this, 'MyEncryptedBucket', {
+      encryption: cdk.aws_s3.BucketEncryption.S3_MANAGED,
+    });
+
     // The code that defines your stack goes here
 
     // example resource
